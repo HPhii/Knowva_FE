a1619bef86013791eff615480ae453bdffcd5647
LeCaoKha <lecaokha123@gmail.com>
2025-08-07 04:18:23 +0700

    fix login, header ui, details, font

--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,7 @@ import { RouterProvider } from "react-router-dom";
 import router from "./router/router";
 import "./index.css";
 import "./i18n";
+import 'antd/dist/reset.css';
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
