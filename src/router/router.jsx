86b2c2da329071250b0a8164b01c5d38d47c5925
Nguyễn Hữu AN <156803278+HuuAn03@users.noreply.github.com>
2025-08-07 01:45:10 +0700

    add Layout,header,footer

--- src/router/router.jsx
+++ src/router/router.jsx
@@ -1,6 +1,8 @@
 import { createBrowserRouter } from "react-router-dom";
 import App from "../App";
 import Home from "../pages/Home/Home";
+import Login from "../pages/Login/Login";
+import Register from "../pages/Register/Register";
 
 const router = createBrowserRouter([
   {
@@ -8,8 +10,9 @@ const router = createBrowserRouter([
     element: <App />,
     children: [
       { index: true, element: <Home /> },
-      // { path: "about", element: <About /> },
-      // { path: "blog", element: <Blog /> },
+        { path: "login", element: <Login /> },
+        { path: "signup", element: <Register /> },
+        // { path: "blog", element: <Blog /> },
       // { path: "contact", element: <Contact /> },
     ],
   },
